57a07577dfd623d7642813599054ff36848e0f33
tb96 <bueschken@team-digital.de>
2020-02-10 09:27:39 +0100

    [UPDATE] Flux Settings

--- installTypo3.sh
+++ installTypo3.sh
@@ -84,7 +84,7 @@ return [
             'offlineMode' => '0',
         ],
         'flux' => [
-            'autoload' => '1',
+            'autoload' => '0',
             'debugMode' => '0',
             'doktypes' => '0,1,4',
             'handleErrors' => '0',
